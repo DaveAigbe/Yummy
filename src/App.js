07a71da6305cc93e2a391fabfc6457d2bf04cbf4
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-08-21 04:09:40 -0500

    - Added Routes - Added Nav Bar functionality/links - Clarified comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Pages from "./Pages/Pages";
+import {BrowserRouter} from "react-router-dom";
+import FoodCategories from "./Components/FoodCategories";
+import React from "react";
 
 function App() {
     return (
         <div className="App">
-            <h1>Yummy</h1>
-            <Pages/>
+            {/*Wrap within browser router so that all pages have access to routing*/}
+            <BrowserRouter>
+                <h1>Yummy</h1>
+                <FoodCategories/>
+                <Pages/>
+            </BrowserRouter>
         </div>
     );
 }
